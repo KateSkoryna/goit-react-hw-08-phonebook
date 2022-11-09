a9ab7a654bd4dda43bf42d95a2bff1f314e3c6ba
Kateryna Skoryna <k.skoryna@gmail.com>
2022-11-09 18:22:55 +0100

    Added basic libs and files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Global } from '@emotion/react';
+import { GlobalStyles } from './GlobalStyles.styled';
+import { Container } from './Container.styled';
+import ContactForm from 'components/ContactForm';
+import Filter from 'components/Filter';
+import ContactList from 'components/ContactList';
+
+const App = () => {
+  return (
+    <>
+      <Global styles={GlobalStyles} />
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </Container>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,43 @@
+import {
+  ContactListBox,
+  ContactListItem,
+  ContactListBtn,
+  ContactListText,
+} from './ContactList.styled';
+import { useEffect } from 'react';
+import { selectContactsByName, selectLoadingStatus } from 'redux/selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchContacts, deleteContact } from 'redux/operations';
+import Loader from 'components/Loader';
+
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContactsByName);
+  const isLoading = useSelector(selectLoadingStatus);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <ContactListBox>
+      {isLoading && <Loader />}
+      {contacts.map(({ id, name, phone }) => (
+        <ContactListItem key={id}>
+          <ContactListText>Name: {name}</ContactListText>
+          <ContactListText>Number: {phone}</ContactListText>
+          <ContactListBtn
+            type="button"
+            onClick={() => {
+              dispatch(deleteContact(id));
+            }}
+          >
+            Delete contact
+          </ContactListBtn>
+        </ContactListItem>
+      ))}
+    </ContactListBox>
+  );
+};
+
+export default ContactList;
